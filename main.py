50cfe7e97d32f2b37c61b2aa97a1b7613fd8c26d
Вадим <educationPyth@gmail.com>
2023-08-06 23:33:07 +0700

    добавил базу данных, переделал под ООп

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import os
 import shutil
+from datetime import datetime
 from sqlite_db import DatabaseUserSettings
+
+
+input_user_path_folder = r'D:\test folder sort'
+input_user_name_database = 'какое-то название'
+current_date = datetime.now()
+formatted_date = current_date.strftime("%d_%m_%Y")
+formatted_new_database = formatted_date
+name_table = f"Settings_{formatted_new_database}"
+
+input_user_name_folder = "video"
+input_user_appropriate = [".avi", "mp4"]
 
 
 class UserInput:
     def __init__(self, name_database: str):
         self.name_database = name_database
+        self.db = DatabaseUserSettings(self.name_database)
         self.path = ''
 
     def set_path(self, folder_path):
@@ -14,63 +27,70 @@ class UserInput:
     def get_path(self):
         return self.path
 
-    def set_sittings(self, name_folder: str, appropriate: str):
-        db = DatabaseUserSettings(self.name_database)
-        with db.conn:
-            db.set_data(name_folder, appropriate)
+    def set_sittings(self, name_folder: str, appropriate: list):
+        with self.db.conn:
+            self.db.set_data(name_folder, appropriate)
+
+    def get_settings(self):
+        with self.db.conn:
+            print(f"Блок кода в классе UserInput get_settings {self.db.get_data()}")
+            return self.db.get_data()
 
     def clear_settings(self):
-        db = DatabaseUserSettings(self.name_database)
-        with db.conn:
-            db.clear_table()
+        with self.db.conn:
+            self.db.clear_table()
 
 
 class CleanerWorktable:
-    def __init__(self, folder_path: str):
+    def __init__(self, folder_path: str, file_types: dict):
+        # get path to folder
         self.full_folder_path = folder_path
+        # get list file of folder
+        self.files = os.listdir(self.full_folder_path)
 
-def sort_folder(folder_path):
-    # get list file of folder
-    files = os.listdir(folder_path)
+        self.file_types = file_types
+        # create dict to story file types  and their corresponding extensions
+        self.sorted_files = {}
 
-    # create dict to story file types  and their corresponding extensions
-    file_types = {
-        "video": [".avi", ".mp4"],
-        "photo": [".jpg", ".jpeg", ".png", ".bmp"],
-        "document": [".doc", ".docx", ".pdf", ".txt"],
-        "music": [".mp3", ".wav"],
-        "other": []
-    }
+    def sort_folder(self):
+        # looping through files and determining their types
+        for file in self.files:
+            if os.path.isfile(os.path.join(self.full_folder_path, file)):
+                file_extension = os.path.splitext(file)[1].lower()
 
-    sorted_files = {
+                for key, values in self.file_types.items():
+                    if file_extension in values:
+                        self.sorted_files.setdefault(key, []).append(file)
+                        break
+                else:
+                    self.sorted_files.setdefault("other", []).append(file)
 
-    }
+        # create folders to files for corresponding extensions
+        for key in self.sorted_files:
+            folder_name = key
+            folder_new_path = os.path.join(self.full_folder_path, folder_name)
+            os.makedirs(folder_new_path, exist_ok=True)
+        # move file to appropriate folder
+            for file in self.sorted_files[key]:
+                file_path = os.path.join(self.full_folder_path, file)
+                file_new_path = os.path.join(folder_new_path, file)
+                shutil.move(file_path, file_new_path)
 
-    # looping through files and determining their types
-    for file in files:
-        if os.path.isfile(os.path.join(folder_path, file)):
-            file_extension = os.path.splitext(file)[1].lower()
+            print(f"Создана папка {folder_name} и перемещено {len(self.sorted_files[key])} файлов")
 
-            for key, values in file_types.items():
-                if file_extension in values:
-                    sorted_files.setdefault(key, []).append(file)
-                    break
-            else:
-                sorted_files.setdefault("other", []).append(file)
 
-    # create folders to files for corresponding extensions
-    for key in sorted_files:
-        folder_name = key
-        folder_new_path = os.path.join(folder_path, folder_name)
-        os.makedirs(folder_new_path, exist_ok=True)
-    # move file to appropriate folder
-        for file in sorted_files[key]:
-            file_path = os.path.join(folder_path, file)
-            file_new_path = os.path.join(folder_new_path, file)
-            shutil.move(file_path, file_new_path)
+def get_data_user():
+    user = UserInput(name_table)
+    user.set_path(input_user_path_folder)
+    user.set_sittings(input_user_name_folder, input_user_appropriate)
 
-        print(f"Создана папка {folder_name} и перемещено {len(sorted_files[key])} файлов")
+    print(user.get_settings())
+    return user.get_settings()
+
+
+def main():
+    get_data_user()
 
 
 if __name__ == "__main__":
-    sort_folder('')
+    main()
--- sqlite_db.py
+++ sqlite_db.py
@@ -1,23 +1,26 @@
 import sqlite3
+import json
 
 
 class DatabaseUserSettings:
-    def __init__(self, database_name):
+    def __init__(self, database_name: str):
         self.conn = sqlite3.connect(database=database_name)
         self.cursor = self.conn.cursor()
-        self.cursor.execute("""CREATE TABLE IF NOT EXISTS user_settings(
-                                path TEXT,
-                                name_folder TEXT,
-                                appropriate_file TEXT
-                                """
+        self.cursor.execute(f"""CREATE TABLE IF NOT EXISTS user_settings(
+                                settings_id INTEGER PRIMARY KEY,
+                                settings_name_folder TEXT,
+                                settings_appropriate_file TEXT
+                                )"""
                             )
 
     def __del__(self):
         self.conn.close()
 
-    def set_data(self, name_folder, appropriate_file):
-        self.cursor.execute("INSERT INTO user_settings (path, name_folder, appropriate_file) VALUES (?, ?, ?)",
-                            (name_folder, appropriate_file))
+    def set_data(self, name_folder: str, appropriate_file: list):
+        serialized_data = json.dumps(appropriate_file)
+        self.cursor.execute("INSERT INTO user_settings"
+                            "(settings_name_folder, settings_appropriate_file) VALUES (?, ?)",
+                            (name_folder, serialized_data))
         self.conn.commit()
 
     def get_data(self):
